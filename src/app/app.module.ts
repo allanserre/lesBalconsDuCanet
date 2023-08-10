e36327fdb5ece6a0d25fdac91d721ff03c12a881
allan-serre <allan.serre@etu.univ-lehavre.fr>
2023-08-10 02:25:36 +0200

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAnalytics, getAnalytics, ScreenTrackingService, UserTrackingService } from '@angular/fire/analytics';
+import { HeaderComponent } from './components/header/header.component';
+import { NavbarLeftComponent } from './components/navbar-left/navbar-left.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavbarLeftComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAnalytics(() => getAnalytics())
   ],
-  providers: [],
+  providers: [
+    ScreenTrackingService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    projectId: 'lesbalconsducanet',
+    appId: '1:140641221101:web:582182834c060743168fe4',
+    storageBucket: 'lesbalconsducanet.appspot.com',
+    apiKey: 'AIzaSyD2qa04OZ1lnlbuKvR_wBy2Da0eGWOurHE',
+    authDomain: 'lesbalconsducanet.firebaseapp.com',
+    messagingSenderId: '140641221101',
+    measurementId: 'G-2ZYY8Q9ZZ8',
+  },
+};
